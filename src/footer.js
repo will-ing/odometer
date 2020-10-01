e006a682c952be981ff9e42346c398d300ff888a
will-ing <will-ing@github.com>
2020-09-30 21:56:26 -0700

    finished lab

---
+++ src/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Footer = ({ trademark }) => (
+    < footer >
+        <p>{trademark}</p>
+    </footer >
+)
+
+
+
+export default Footer
---
+++ src/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Header = ({ text }) => (
+    <header className="App-header">
+        <p>This is the header, my name is {text}</p>
+    </header>
+)
+
+
+
+
+export default Header
+
